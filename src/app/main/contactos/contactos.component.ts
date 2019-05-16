cb9d3f6cefd42e4faf93bf8a7ddab40713ce9a34
Juan Cruz <etcheverryjuancruz@gmail.com>
2019-05-16 16:58:01 -0300

    Agregar lista de contactos a contactos

--- src/app/main/contactos/contactos.component.ts
+++ src/app/main/contactos/contactos.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { fuseAnimations } from '@fuse/animations';
 
 @Component({
   selector: 'app-contactos',
   templateUrl: './contactos.component.html',
-  styles: []
+  styles: ['./contactos.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  animations   : fuseAnimations
 })
 export class ContactosComponent implements OnInit {
+
+  contactsTitle: string = "Contacts"
 
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  toggleSidebar(name): void
+  {
+      //this._fuseSidebarService.getSidebar(name).toggleOpen();
   }
 
 }
--- src/app/main/contactos/contactos.module.ts
+++ src/app/main/contactos/contactos.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ContactosRoutingModule } from './contactos-routing.module';
 import { ContactosComponent } from './contactos.component';
 import { ListContactosComponent } from './list-contactos/list-contactos.component';
 
 import { FuseSharedModule } from '@fuse/shared.module';
+
+import { MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatFormFieldModule, 
+         MatIconModule, MatInputModule, MatMenuModule, MatRippleModule, MatTableModule, MatToolbarModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,18 @@ import { FuseSharedModule } from '@fuse/shared.module';
   imports: [
     CommonModule,
     ContactosRoutingModule,
-    
+
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    MatRippleModule,
+    MatTableModule,
+    MatToolbarModule,
+
     FuseSharedModule
   ]
 })
