f1de1cf1635dd2a0ce92f48ac9087a207cf0397a
Juan Cruz <etcheverryjuancruz@gmail.com>
2019-05-16 15:02:54 -0300

    Crear lista de contactos

--- src/app/main/contactos/contactos.module.ts
+++ src/app/main/contactos/contactos.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactosRoutingModule } from './contactos-routing.module';
 import { ContactosComponent } from './contactos.component';
+import { ListContactosComponent } from './list-contactos/list-contactos.component';
+
+import { FuseSharedModule } from '@fuse/shared.module';
 
 @NgModule({
   declarations: [
-    ContactosComponent
+    ContactosComponent,
+    ListContactosComponent
   ],
   imports: [
     CommonModule,
-    ContactosRoutingModule
+    ContactosRoutingModule,
+    
+    FuseSharedModule
   ]
 })
 export class ContactosModule { }
