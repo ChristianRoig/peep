3c1c50241344fd0420cfb6734dcf96d1ae67bef7
victor <g.victor.juan@gmail.com>
2019-05-22 16:12:03 -0300

    elimino tabs que no se utilizan

--- src/app/main/perfil/perfil.module.ts
+++ src/app/main/perfil/perfil.module.ts
@@ -9,8 +9,7 @@ import { FuseSharedModule } from '@fuse/shared.module';
 import { PerfilInfoComponent } from './tabs/perfil-info/perfil-info.component';
 import { PerfilInfoLegComponent } from './tabs/perfil-info-leg/perfil-info.component';
 import { PerfilInfoSegComponent } from './tabs/perfil-info-seg/perfil-info.component';
-import { PerfilPreferenciasComponent } from './tabs/perfil-preferencias/perfil-preferencias.component';
-import { PerfilImagenesComponent } from './tabs/perfil-imagenes/perfil-imagenes.component';
+
 
 import { TranslateModule } from '@ngx-translate/core';
 
@@ -19,9 +18,7 @@ import { TranslateModule } from '@ngx-translate/core';
     PerfilComponent,
     PerfilInfoComponent,
     PerfilInfoLegComponent,
-    PerfilInfoSegComponent,
-    PerfilPreferenciasComponent,
-    PerfilImagenesComponent
+    PerfilInfoSegComponent,    
   ],
   imports: [
     CommonModule,
