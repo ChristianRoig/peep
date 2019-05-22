783eab7cc25b87dc82f0b2c2ce6180a4da80d5f9
victor <g.victor.juan@gmail.com>
2019-05-22 16:14:14 -0300

    coloco el llamado al servicio en el componente padre

--- src/app/main/perfil/perfil.component.ts
+++ src/app/main/perfil/perfil.component.ts
@@ -4,6 +4,9 @@ import { FuseTranslationLoaderService } from '@fuse/services/translation-loader.
 
 import { locale as english } from './i18n/en';
 import { locale as spanish } from './i18n/es';
+import { PerfilService } from './perfil.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'perfil',
@@ -13,14 +16,36 @@ import { locale as spanish } from './i18n/es';
   animations   : fuseAnimations
 })
 export class PerfilComponent implements OnInit {
+  
+  info: any;
+
+  // Private
+  private _unsubscribeAll: Subject<any>;
 
   constructor(
-    private _fuseTranslationLoaderService: FuseTranslationLoaderService
+    private _fuseTranslationLoaderService: FuseTranslationLoaderService,
+    private _profileService: PerfilService
   ) {
     this._fuseTranslationLoaderService.loadTranslations(spanish, english);
+
+    this._unsubscribeAll = new Subject();
    }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this._profileService.infoOnChanged
+      .pipe(takeUntil(this._unsubscribeAll))
+      .subscribe(info => {
+        this.info = info;
+      });
+  }
+
+  /**
+   * On destroy
+   */
+  ngOnDestroy(): void {
+    // Unsubscribe from all subscriptions
+    this._unsubscribeAll.next();
+    this._unsubscribeAll.complete();
   }
 
 }
