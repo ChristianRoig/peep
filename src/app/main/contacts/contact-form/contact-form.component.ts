9c054372a0e0c9ac5238e2cd298433ea8a726d62
Juan <etcheverryjuancruz@gmail.com>
2019-07-16 10:16:32 -0300

    Modificar formulario de contacto

--- src/app/main/contacts/contact-form/contact-form.component.ts
+++ src/app/main/contacts/contact-form/contact-form.component.ts
@@ -1,9 +1,14 @@
-import { Component, Inject, ViewEncapsulation } from '@angular/core';
+import { Component, Inject, ViewEncapsulation, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
 import { Contact } from 'app/main/contacts/contact.model';
-import { TitleCasePipe } from '@angular/common';
+import { ContactsService } from '../contacts.service';
+
+export interface Option {
+    value: string;
+    viewValue: string;
+  }
 
 @Component({
     selector     : 'contacts-contact-form-dialog',
@@ -12,13 +17,30 @@ import { TitleCasePipe } from '@angular/common';
     encapsulation: ViewEncapsulation.None
 })
 
-export class ContactsContactFormDialogComponent
+export class ContactsContactFormDialogComponent implements OnInit
 {
     action: string;
     contact: Contact;
     contactForm: FormGroup;
     dialogTitle: string;
+    contactName: string;
+    copy: boolean = false;
 
+    estados: Option[] = [
+        {value: 'Responsable Inscripto', viewValue: 'Responsable Inscripto'},
+        {value: 'Monotributista',        viewValue: 'Monotributista'},
+        {value: 'Consumidor Final',      viewValue: 'Consumidor Final'},
+        {value: 'Exento',                viewValue: 'Exento'},
+        {value: 'Otra',                  viewValue: 'Otra'}
+      ];
+
+    generos: Option[] = [
+        {value: 'Neutro',  viewValue: 'Neutro'},
+        {value: 'Hombre',  viewValue: 'Hombre'},
+        {value: 'Mujer',   viewValue: 'Mujer'},
+        {value: 'Empresa', viewValue: 'Empresa'},
+    ];
+    
     /**
      * Constructor
      *
@@ -29,25 +51,60 @@ export class ContactsContactFormDialogComponent
     constructor(
         public matDialogRef: MatDialogRef<ContactsContactFormDialogComponent>,
         @Inject(MAT_DIALOG_DATA) private _data: any,
+        private _contactsService: ContactsService,
         private _formBuilder: FormBuilder
     )
     {
         // Set the defaults
         this.action = _data.action;
-
+        
+        
         if ( this.action === 'edit' )
         {
             this.dialogTitle = 'Editar Proveedor';
             this.contact = _data.contact; 
+            this.contactForm = this.createContactForm();                  
         }
         else
         {
             this.dialogTitle = 'Nuevo Proveedor';
             this.contact = new Contact({});
+            this._contactsService.initContacto(this.contact);
+            this.contactForm = this.createContactForm();                  
+            this._contactsService.createRequestNewCodigo("7ideas", "Proveedores")
+            .subscribe((response : any) => {
+                this.contact.cod = response._body;          
+                this.contactForm.controls['cod'].setValue(this.contact.cod); //setea el cod que 
+            });
+/*             this._contactsService.getGastosByName(this.proveedor.nombre_corto).then((value) => {
+                this.gastos = value;
+                });  */
         }
 
-        this.contactForm = this.createContactForm();
     }
+
+    ngOnInit(): void {
+        this.contactName = this.contact.nombre_corto;
+    }
+
+    NameOnChanges( newValue:string) : void {
+        this.contactName = newValue;
+    }
+
+    uploadImage(event): void {
+        const fileList: FileList = event.target.files;
+        if (fileList.length > 0) {
+            let fileReader = new FileReader();
+            let file = fileList[0];
+            fileReader.onload = (e) => {
+                console.log(fileReader.result);
+                let img = fileReader.result;
+                this.contact.file_link = img.toString();
+                this.contactForm.controls['file_link'].setValue(img.toString());
+            }
+            fileReader.readAsDataURL(file);
+        }
+      }
 
     // -----------------------------------------------------------------------------------------------------
     // @ Public methods
@@ -61,25 +118,26 @@ export class ContactsContactFormDialogComponent
     createContactForm(): FormGroup
     {
         return this._formBuilder.group({
-
-            name    : [this.contact.name],
-            company : [this.contact.company],
-            email   : [this.contact.email],
-            phone   : [this.contact.telefono],
-            address : [this.contact.direccion],
-            notes   : [this.contact.notes]
-/*             id      : [this.contact.id],
-            name    : TitleCasePipe.apply([this.contact.name]),
-            lastName: [this.contact.lastName],
-            avatar  : [this.contact.avatar],
-            nickname: [this.contact.nickname],
-            company : [this.contact.company],
-            jobTitle: [this.contact.jobTitle],
-            email   : [this.contact.email],
-            phone   : [this.contact.telefono],
-            address : [this.contact.direccion],
-            birthday: [this.contact.birthday],
-            notes   : [this.contact.notes] */
+            id : [this.contact.id],
+            cod : [this.contact.cod], 
+            propietario : [this.contact.propietario],
+            modulo : [this.contact.modulo],
+            categoria : [this.contact.categoria],
+            etiqueta : [this.contact.etiqueta],  
+            nombre_corto : [this.contact.nombre_corto],
+            nombre : [this.contact.nombre],
+            correo  : [this.contact.correo],
+            telefono : [this.contact.telefono],
+            domicilio : [this.contact.domicilio],
+            localidad : [this.contact.localidad],
+            cond_iva : [this.contact.cond_iva],
+            cuit : [this.contact.doc_nro],
+            genero : [this.contact.genero],
+            notas : [this.contact.notas],
+            file_link : [this.contact.file_link],
+            activo : [this.contact.activo],
+            predefinido : [this.contact.predefinido],
+            estado : [this.contact.estado]
         });
     }
 }
