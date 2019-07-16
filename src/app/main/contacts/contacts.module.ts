88cd6082a3e5095d4b91a2e4167273c9867480d2
Juan <etcheverryjuancruz@gmail.com>
2019-07-16 10:28:35 -0300

    Agregar modulo de Select al module

--- src/app/main/contacts/contacts.module.ts
+++ src/app/main/contacts/contacts.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {
-    MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatFormFieldModule, MatIconModule, MatInputModule, MatMenuModule, MatRippleModule, MatTableModule, MatToolbarModule, MatTabsModule
+    MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatFormFieldModule, MatIconModule, MatInputModule, MatMenuModule, MatRippleModule, MatTableModule, MatToolbarModule, MatTabsModule, MatSelectModule
 } from '@angular/material';
 
 import { FuseSharedModule } from '@fuse/shared.module';
@@ -12,7 +12,8 @@ import { ContactsService } from 'app/main/contacts/contacts.service';
 import { ContactsContactListComponent } from 'app/main/contacts/contact-list/contact-list.component';
 import { ContactsSelectedBarComponent } from 'app/main/contacts/selected-bar/selected-bar.component';
 import { ContactsMainSidebarComponent } from 'app/main/contacts/sidebars/main/main.component';
-import { ContactsContactFormDialogComponent } from 'app/main/contacts/contact-form/contact-form.component';
+//import { ContactsContactFormDialogComponent } from 'app/main/contacts/contact-form/contact-form.component';
+import { ContactsContactFormDialogComponent } from 'app/main/contacts/contact-form/contact-form-cp.component';
 import { ContactViewComponent } from './contact-view/contact-view.component';
 import { ContactInfoComponent } from './contact-view/tabs/contact-info/contact-info.component';
 import { ContactGastosComponent } from './contact-view/tabs/contact-gastos/contact-gastos.component';
@@ -59,6 +60,7 @@ const routes: Routes = [
         MatMenuModule,
         MatRippleModule,
         MatTableModule,
+        MatSelectModule,
         MatToolbarModule,
         MatTabsModule,
 
@@ -68,6 +70,9 @@ const routes: Routes = [
     ],
     providers      : [
         ContactsService
+    ],
+    exports:[
+     //   ContactsContactFormDialogComponent
     ],
     entryComponents: [
         ContactsContactFormDialogComponent
